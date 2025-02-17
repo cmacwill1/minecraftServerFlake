11e3e1d7a17ef0dc8d5c30d84aaf8db77ef74522
cmacwill1 <charles.macwilliams@gmail.com>
2025-02-16 21:59:12 -0500

    trying nix-minecraft

--- minecraftServer.nix
+++ minecraftServer.nix
@@ -1,6 +1,29 @@
-{ inputs, ... }:
+{ inputs, pkgs, lib, ... }:
 
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    servers = {
+      fabric-server = {
+	enable = true;
+	package = pkgs.fabricServers.fabric-1_21_4.override {
+	  loaderVersion = "0.16.10";
+	}; 
+	serverProperties = {
+	  server-port = 25566;
+	};
+
+	symlinks = {
+	  "mods" = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
+	    Lithium = fetchurl { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/QCuodIia/lithium-fabric-0.14.7%2Bmc1.21.4.jar"; };
+	  });
+	};
+      };
+    };
+  };
 }
