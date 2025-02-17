5baae724d802eef868f9f5b293a53fade11e8955
cmacwill1 <charles.macwilliams@gmail.com>
2025-02-16 22:57:03 -0500

    fake sha

--- minecraftServer.nix
+++ minecraftServer.nix
@@ -19,7 +19,7 @@
 	};
 	symlinks = {
 	  mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
-	    Lithium = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/3WOjLgFJ/fabric-api-0.116.1%2B1.21.4.jar"; sha512 = "e5f3c3431b96b281300dd118ee523379ff6a774c0e864eab8d159af32e5425c915f8664b1"; };
+	    Lithium = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/3WOjLgFJ/fabric-api-0.116.1%2B1.21.4.jar"; sha512 = lib.fakeSha512; };
 	  });
 	};
       };
